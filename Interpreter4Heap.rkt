991e828814e64e863203e27f1046c79c8fdb5f74
Kienan <kienan@saucybytes.com>
2020-04-29 03:53:24 -0500

    Added some helpful documentation.

--- Interpreter4Heap.rkt
+++ Interpreter4Heap.rkt
@@ -9,6 +9,16 @@
 
 ;----------------------------------------------------------------------;
 ; HEAP
+; A beautiful, fast heap implementation. No cheaty non-functional-ness here!
+; It takes a state-of-the art patent-pending new approach to memory management:
+; Memory, once allocated, can never be freed.
+; (some may recognize this as the memory management approach used by Google Chrome)
+; Features all of the lightning-fast features a modern programmer needs:
+;  - Dereference pointers in subexponential time!
+;  - Allocate memory in O(1) time (for variable values of 1)!
+;  - Floating point heap pointers are supported and encouraged!
+;  - A free *BONUS* ultra-fast pointer to the number seven (if you can find it)!
+;  - Error checking for several possible error states!
 
 ; Returns the heap from the given state
 (define state_getheap
@@ -23,7 +33,7 @@
 ; Creates a new heap in the given state at the current layer.
 (define state_createheap
   (lambda (state)
-    (state_update_impl 'super '(1) (state_declare_impl 'super state))))
+    (state_update_impl 'super '(2 7) (state_declare_impl 'super state))))
 
 ; Returns the size of the given heap.
 (define heap_getsize
@@ -46,19 +56,19 @@
 
 ; Get the pointer to the object created on the given heap with the next heap_add_resultheap call.
 ; Test case: (heap_add_resultptr (state_createheap '(())))
-;         -> 1
-; Test case: (heap_add_resultptr (heap_add_resultstate (state_createheap '(())) 'a))
 ;         -> 2
+; Test case: (heap_add_resultptr (heap_add_resultstate (state_createheap '(())) 'a))
+;         -> 3
 (define heap_add_resultptr
   (lambda (state)
     (heap_getsize (state_getheap state))))
 
 ; Dereference a heap pointer.
-; Test case: (ptr_dereference (heap_add_resultstate (state_createheap '(())) 'a) 1)
-;         -> 'a
-; Test case: (ptr_dereference (heap_add_resultstate (heap_add_resultstate (state_createheap '(())) 'a) 'b) 1)
+; Test case: (ptr_dereference (heap_add_resultstate (state_createheap '(())) 'a) 2)
 ;         -> 'a
 ; Test case: (ptr_dereference (heap_add_resultstate (heap_add_resultstate (state_createheap '(())) 'a) 'b) 2)
+;         -> 'a
+; Test case: (ptr_dereference (heap_add_resultstate (heap_add_resultstate (state_createheap '(())) 'a) 'b) 3)
 ;         -> 'b
 (define ptr_dereference
   (lambda (state ptr)
